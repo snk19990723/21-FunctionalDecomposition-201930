f31222433a0b0fcafc2130f258777adda94a5451
zoud <zoud@rose-hulman.edu>
2019-05-08 08:07:42 -0400

    DONE

--- src/m1_hangman.py
+++ src/m1_hangman.py
@@ -1,10 +1,26 @@
 """
 Hangman.
 
-Authors: PUT_YOUR_NAME_HERE and YOUR_PARTNERS_NAME_HERE.
+Authors: Deng Zou and Haozhe Wu.
 """  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 # TODO: 2. Implement Hangman using your Iterative Enhancement Plan.
-
+import random
 ####### Do NOT attempt this assignment before class! #######
+def word():
+    with open('words.txt') as f:
+        f.readline()
+        string = f.read()
+        print(string)
+
+    words=string.split()
+    print(words)
+    r=random.randrange(0,len(words))
+    item=words[r]
+    print(item)
+
+def get_guess(x):
+    string= input(prompt=x)
+    return string
+
 
